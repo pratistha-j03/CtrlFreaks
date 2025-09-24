9525fbc5bc93c6386f6ad84407a1500a8ec2a9b1
iByteRajan <pwianrajan@gmail.com>
2025-09-24 23:30:48 +0530

    Update adminRouter.js

--- Server/router/adminRouter.js
+++ Server/router/adminRouter.js
@@ -1,1 +1,8 @@
 // admin Router
+import { Router } from "express";
+import postEvent from "../controllers/adminControllers.js";
+
+const router=Router();
+router.route("/event").post(postEvent);
+
+export default router;
